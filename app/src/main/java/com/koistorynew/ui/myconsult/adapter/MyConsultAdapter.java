bed1251bedc951f70e1bb366f6d975eeee37bafe
nhantam2502 <nhantamhd@gmail.com>
2024-10-21 11:18:17 +0700

    fetch api get comment for my consult and post comment in post

--- app/src/main/java/com/koistorynew/ui/myconsult/adapter/MyConsultAdapter.java
+++ app/src/main/java/com/koistorynew/ui/myconsult/adapter/MyConsultAdapter.java
@@ -66,7 +66,7 @@ public class MyConsultAdapter extends RecyclerView.Adapter<MyConsultAdapter.Cons
 
         holder.commentButton.setOnClickListener(view -> {
             Intent intent = new Intent(view.getContext(), MyConsultCommentActivity.class);
-            intent.putExtra("CONSULT_ID", consult.getId()); // Assuming you want to pass consult ID
+            intent.putExtra("POST_ID", consult.getId()); // Assuming you want to pass consult ID
             view.getContext().startActivity(intent);
         });
     }
